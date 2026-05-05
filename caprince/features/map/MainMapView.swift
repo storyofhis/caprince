c9f3694dc0da1768d9e62639d29f2356ce661bda
Clarawita <clarawita28@gmail.com>
2026-05-05 17:21:27 +0700

    fix: week card view

--- caprince/features/map/MainMapView.swift
+++ caprince/features/map/MainMapView.swift
@@ -1,16 +1,23 @@
 import SwiftUI
 
 struct MainMapView: View {
-    @StateObject private var viewModel = RunTrackerViewModel()
-    var trainingDay: TrainingDay?
+    @StateObject private var viewModel: RunTrackerViewModel
     var onRunComplete: () -> Void
+    
+    init(week: TrainingWeek? = nil, day: TrainingDay? = nil, onRunComplete: @escaping () -> Void) {
+        let vm = RunTrackerViewModel()
+        vm.currentWeek = week
+        vm.currentDay = day
+        _viewModel = StateObject(wrappedValue: vm)
+        self.onRunComplete = onRunComplete
+    }
     
     var body: some View {
 //        Group {
             if viewModel.sessionState == .finished {
                 FinishRunView(viewModel: viewModel, onRunComplete: onRunComplete)
             } else {
-                ActiveRunView(viewModel: viewModel, trainingDay: trainingDay)
+                ActiveRunView(viewModel: viewModel, trainingDay: viewModel.currentDay)
             }
 //        }
 //        .foregroundStyle(.black)
