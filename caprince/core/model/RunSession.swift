848887b2b3c49f24740a7acb167c09647af03ab9
storyofhis <azizi.maula@gmail.com>
2026-05-05 10:00:49 +0700

    feat: add map for history activities

--- caprince/core/model/RunSession.swift
+++ caprince/core/model/RunSession.swift
@@ -16,15 +16,20 @@ final class RunSession {
     var timeElapsed: TimeInterval
     var distance: Double
     var averagePace: String
+    var steps: Int
+    var calories: Double
+    var routeData: Data?
     
-    init(id: UUID = UUID(), date: Date = Date(), programName: String, timeElapsed: TimeInterval, distance: Double, averagePace: String) {
+    init(id: UUID = UUID(), date: Date = Date(), programName: String, timeElapsed: TimeInterval, distance: Double, averagePace: String, steps: Int, calories: Double, routeData: Data? = nil) {
         self.id = id
         self.date = date
         self.programName = programName
         self.timeElapsed = timeElapsed
         self.distance = distance
         self.averagePace = averagePace
+        self.steps = steps
+        self.calories = calories
+        self.routeData = routeData
     }
 }
-
 
